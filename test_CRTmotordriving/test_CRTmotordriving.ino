095c7684ad43b209a15a4d329046107bc08cbcd2
Vinay Kancharla <116471509+Sigma24@users.noreply.github.com>
2023-03-21 17:47:08 -0400

    Format test_CRTmotordriving

--- test_CRTmotordriving/test_CRTmotordriving.ino
+++ test_CRTmotordriving/test_CRTmotordriving.ino
@@ -2,8 +2,8 @@ const int R_EN = 4;
 const int L_EN = 7;
 const int R_PWM = 3;
 const int L_PWM = 6;
+
 void setup() {
-  // put your setup code here, to run once:
   pinMode(R_PWM, OUTPUT);
   pinMode(L_PWM, OUTPUT);
   pinMode(R_EN, OUTPUT);
@@ -11,8 +11,8 @@ void setup() {
   digitalWrite(R_EN,LOW);
   digitalWrite(L_EN,LOW);
 }
+
 void loop() {
-  // put your main code here, to run repeatedly:
   digitalWrite(R_EN, HIGH);
   analogWrite(R_PWM, 50);
   delay(200);
