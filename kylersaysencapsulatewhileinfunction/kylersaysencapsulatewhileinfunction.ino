4d2a8d61c8c5b76d6c6a9c5e8491cce38d26f0bc
Kyler <kylerrosen@gmail.com>
2023-04-25 20:39:37 -0400

    current program has working limit switch and initial 0-set

--- kylersaysencapsulatewhileinfunction/kylersaysencapsulatewhileinfunction.ino
+++ kylersaysencapsulatewhileinfunction/kylersaysencapsulatewhileinfunction.ino
@@ -26,9 +26,15 @@ const int MOTOR_IN2 = 21;
 const int SWITCH = 0;
 
 int readValue; 
+int state;
+bool retractedFlaps;
+
+bool state_printout = false;
 
 Encoder encoder(Encoder_A, Encoder_B); 
 void setup() {
+  state = 0;
+  retractedFlaps = (digitalRead(SWITCH) == HIGH);
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(Encoder_A, INPUT);
@@ -50,6 +56,7 @@ void setup() {
     //status = bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID);
     unsigned status;
   status = bmp.begin();
+
   if (!status) {
     Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
                       "try a different address!"));
@@ -71,12 +78,178 @@ void setup() {
 }
 
 void loop() {
-  while (digitalRead(SWITCH) == LOW) {
+  if (digitalRead(SWITCH) == HIGH) { 
+    if (!retractedFlaps){
+      retractedFlaps = true;
+      encoder.write(0);
+      digitalWrite(MOTOR_IN1, LOW);
+      digitalWrite(MOTOR_IN2, HIGH);
+      delay(30);
+    }
+    digitalWrite(MOTOR_IN1, LOW);
+    digitalWrite(MOTOR_IN2, LOW);
+    delay(500);
+    readValue = encoder.read(); 
+    Serial.print("Encoder = ");
+    Serial.print(readValue);
+    Serial.println();
+    if(state == 0){ 
+      state++;
+    }
+    
     delay(10);
   }
-  // put your main code here, to run repeatedly:
-  WHATITDOBBG(4750); 
+  else{
+    retractedFlaps = false;
+    switch(state) {
+    case 0:
+      calibrate();
+      break;
+    case 1:
+      delay(5000);
+      state++;
+      break;
+    // more cases can be added here
+    default:
+      statePrintout();
+      oscillate(4750); 
+      delay(10);
+      break;
+    }
+  }
+}
 
+void statePrintout(){
+  if (state_printout){
+    Serial.print(F("Temperature = "));
+    Serial.print(bmp.readTemperature());
+    Serial.println(" *C");
+
+    Serial.print(F("Pressure = "));
+    Serial.print(bmp.readPressure());
+    Serial.println(" Pa");
+
+    Serial.print(F("Approx altitude = "));
+    Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
+    Serial.println(" m");
+
+    Serial.println(); 
+  }
+  readValue = encoder.read(); 
+  Serial.print("Encoder = ");
+  Serial.print(readValue);
+  Serial.println();
+}
+
+void speed_to(int location){
+  readValue = encoder.read();
+  if (readValue > location){
+    delay(10); 
+    digitalWrite(MOTOR_IN1, LOW);
+    delay(10);
+    digitalWrite(MOTOR_IN2, HIGH);
+  }
+  else {
+    delay(10); 
+    digitalWrite(MOTOR_IN1, HIGH);
+    delay(10);
+    digitalWrite(MOTOR_IN2, LOW);
+  }
+}
+
+bool go_counterclockwise = true;
+
+void oscillate(int range){
+  readValue = encoder.read(); 
+  if (readValue > range) {
+    go_counterclockwise = true;
+  }
+  if (readValue < 0){
+    go_counterclockwise = false;
+  }
+  Serial.println(go_counterclockwise);
+  if (go_counterclockwise){
+    speed_to(0);
+  }
+  else {
+    speed_to(range);
+  }
+} 
+
+void calibrate(){
+  delay(10);
+  digitalWrite(MOTOR_IN2, LOW);
+  delay(10);
+  digitalWrite(MOTOR_IN1, HIGH);
+}
+
+void PWM_test(int range){
+  delay(10);
+  analogWrite(MOTOR_IN2, 0);
+  delay(10);
+  analogWrite(MOTOR_IN1, 255);  
+  Serial.print(F("Temperature = "));
+}
+
+void analog_test(int range){  
+    // ramp up forward
+  delay(10);
+  analogWrite(MOTOR_IN2, 0);
+  delay(10);
+  analogWrite(MOTOR_IN1, 255);  
+  readValue = encoder.read(); 
+  while (readValue < range) {
+    Serial.print(F("Temperature = "));
+    Serial.print(bmp.readTemperature());
+    Serial.println(" *C");
+
+    Serial.print(F("Pressure = "));
+    Serial.print(bmp.readPressure());
+    Serial.println(" Pa");
+
+    Serial.print(F("Approx altitude = "));
+    Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
+    Serial.println(" m");
+
+    Serial.println();
+    delay(1);
+    readValue = encoder.read(); 
+    Serial.print("Encoder = ");
+    Serial.print(readValue);
+    Serial.println(); 
+    delay(1); 
+  }
+  //hit one rotation; 
+
+  delay(10); 
+  analogWrite(MOTOR_IN1, 0);
+  delay(10);
+  analogWrite(MOTOR_IN2, 255);
+  // ramp down forward
+  while (readValue > 0) {
+    Serial.print(F("Temperature = "));
+    Serial.print(bmp.readTemperature());
+    Serial.println(" *C");
+
+    Serial.print(F("Pressure = "));
+    Serial.print(bmp.readPressure());
+    Serial.println(" Pa");
+
+    Serial.print(F("Approx altitude = "));
+    Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
+    Serial.println(" m");
+
+    Serial.println();
+    delay(1);
+    readValue = encoder.read(); 
+    Serial.print("Encoder = ");
+    Serial.print(readValue);
+    Serial.println(); 
+    delay(1);
+
+  }
+  analogWrite(MOTOR_IN1, 0);
+  analogWrite(MOTOR_IN2, 0);
 }
 
 void WHATITDOBBG(int range){  
